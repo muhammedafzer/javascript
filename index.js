4d28a7b4be5f427ceac215f8971209d0df01f445
Afzer <muhammedafzer777@gmail.com>
2025-07-27 10:35:56 +0530

    varialble

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log(`hello everyone`);
+let cgpa = 8.61;
+let age = 19
+let college_fee = 50000;
+
+
+console.log("you are eligible for scholarship");
+console.log("Your CGPA is: " + cgpa);   
+
+
+let firstname ="ahmed";
+let lastname = "khan";
+
+console.log("firstname is : " + firstname + " " + lastname);
+
+let online = true;
+
+console.log(`Bro is online: ${online}`);
+
+let offine = false;
+let forsale = false;
+let issold = true; 
+console.log(`Bro is offline: ${offine}`);
+console.log(`Item is for sale: ${forsale}`);
+console.log(`Item is sold: ${issold}`);
