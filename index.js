346d75574b8d39832629282ac321e7987c250be8
Afzer <muhammedafzer777@gmail.com>
2025-07-27 14:49:38 +0530

    userinput

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-let cgpa = 8.61;
-let age = 19
-let college_fee = 50000;
+let username;
 
+document.getElementById("mySubmit").onclick = function()
+  {
 
-console.log("you are eligible for scholarship");
-console.log("Your CGPA is: " + cgpa);   
-
-
-let firstname ="ahmed";
-let lastname = "khan";
-
-console.log("firstname is : " + firstname + " " + lastname);
-
-let online = true;
-
-console.log(`Bro is online: ${online}`);
-
-let offine = false;
-let forsale = false;
-let issold = true; 
-console.log(`Bro is offline: ${offine}`);
-console.log(`Item is for sale: ${forsale}`);
-console.log(`Item is sold: ${issold}`);
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myH1").textContent = `Hello    ${username}`;}
